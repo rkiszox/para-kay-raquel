9ed5d830a5dbfca1739349b6b450d3f1d67751cc
rkiszox <rjprinc.3o@gmail.com>
2026-09-14 21:43:41 +0800

    Add files via upload

---
+++ calc.css
@@ -0,0 +1,83 @@
+body{
+    margin: 0;
+    background-color: #141414;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    width: 100vw;
+}
+
+#calc_box {
+    width: 350px;
+    background-color: rgb(28, 27, 27);
+    padding: 25px;
+    border-radius: 15px;
+}
+
+#display_screen{
+    width: 100%;
+    height: 15vh;
+    background-color: rgb(50, 49, 49);
+    color: white;
+    box-sizing: border-box;
+    font-size: 23px;
+    font-weight: bolder;
+    padding: 10px;
+    border-radius: 15px;
+    border: none;
+    margin-bottom: 25px;
+}
+
+.buttons{
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 10px
+}
+
+.buttons button:hover{
+    background:rgb(82, 81, 81);
+}
+
+.buttons button:not(.operator):hover{
+    background:rgb(82, 81, 81);
+}
+
+.buttons button:not(.operator){
+    height: 60px;
+    background-color: rgb(50, 49, 49);
+    border: none;
+    border-radius: 10px;
+    font-size: 20px;
+    color: white;
+    font-weight: 600;
+}
+
+#equals{
+    grid-column: span 2;
+    color: white;
+    background-color: orange;
+}
+
+#equals:hover{
+    background-color: rgb(255, 188, 63);
+}
+.operator{
+    height: 60px;
+    background-color: rgb(50, 49, 49);
+    border: none;
+    border-radius: 10px;
+    font-size: 20px;
+    font-weight: 600;
+    color: orange
+}
+
+@keyframes bw_flash {
+    0%   { background-color: black; color: white; }
+    50%  { background-color: white; color: black; }
+    100% { background-color: black; color: white; }
+}
+
+.change_bg {
+    animation: bw_flash 0.75s infinite;
+}
